0577ef20cf033a117996d73375ba49aedaae343a
Ashish Ahuja <ashishahuja124@gmail.com>
2022-11-10 19:30:30 +0530

    sumZero using multiple pointers

--- src/DSA/Multiple Pointers/sumZero.js
+++ src/DSA/Multiple Pointers/sumZero.js
@@ -9,43 +9,49 @@
 
 // function sumZero(arr) {
 //     for(let i =0; i<arr.length; i++) {
-//         for(let j = 0; j< arr.length; j++) {
+//         for(let j = i+1; j< arr.length; j++) {
 //             if(arr[i] + arr[j] === 0) {
 //                 return [arr[i], arr[j]]
 //             }
 //         }
+//         console.log('No pair found')
+
 //     }
 // }
+//  console.log(sumZero([-3,-2,1,0,1,2,3]));
+
+
 
 // function sumZero(arr) {
-//     for(let val of arr) {
-//         for(let val2 of arr) {
-//             if(val + val2 === 0) {
-//                 return [val, val2]
+//     for(let val1 of arr) {
+//         for (let j=val1+1; j<arr.length; j++) {
+//             if(val1 + arr[j] === 0) {
+//                 console.log(val1, arr[j])
+//                 return (val1, arr[j])
 //             }
 //         }
 //     }
 // }
-//  console.log(sumZero([-3,-2,1,0,2,3]));
+//  console.log(sumZero([-3,-2,1,0])); Time complexity O(n2)
 
+// Using multiple pointers
+////Refactor to two pointers method : TIme complexity: O(n) Space compexity : O(1)
 
-//Refactor to two pointers method : TIme complexity: O(n) Space compexity : O(1)
-
-function sumZero(arr) {
+function isSumZero(arr) {
     let left = 0;
     let right = arr.length - 1;
 
     while(left < right) {
         let sum = arr[left] + arr[right];
-        if(sum == 0) {
-            return [arr[left], arr[right]]
-        }
-        else if(sum > 0 ) {
-            right--
+        if(sum === 0) {
+            return [arr[left], arr[right],'These are the first pair having sum zero']
+        }else if(sum > 0) {
+            right --;
         }
         else {
-            left++;
+            left ++;
         }
     }
+    console.log('no pair')
 }
- console.log(sumZero([-2,1,0,3]))
+console.log(isSumZero([-3,-2,1,0,1,2,3]));
