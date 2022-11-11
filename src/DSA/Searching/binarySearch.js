f656d4b1908375543149d195b05a955de22a03e9
Ashish Ahuja <ashishahuja124@gmail.com>
2022-11-11 16:13:39 +0530

    Binary Search Algo

--- src/DSA/Searching/binarySearch.js
+++ src/DSA/Searching/binarySearch.js
@@ -1,3 +1,13 @@
+//Divide and Conquer technique
+// console.log(searchAlgo([1,2,3,4,,5,6,7,8,9,10,11,12,13,14,15],7)) ==> index 6  output 7
+// min = 0; max = arr.lenght -1
+// midIndex = min + max) / 2
+// if(arr[midIndex] < num) 
+// min = midIndex + 1
+//if(arr[midIndex] > num)
+// min = midIndex - 1
+//else midIndex
+
 function searchAlgo(arr, num) {
     let min = 0;
     let max = arr.length - 1;
