e1863150d9bc1b6c029b6db2e272e5dbe8e68d5c
Ashish Ahuja <ashishahuja124@gmail.com>
2022-11-14 13:41:09 +0530

    two pointer approach for remove dup

--- src/LeetCode/LC26RemoveDup.js
+++ src/LeetCode/LC26RemoveDup.js
@@ -27,3 +27,20 @@ var removeDuplicates = function(nums) {
 };
 var res = removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
 console.log(res);
+
+function unique(arr) {
+    if(arr.length > 0) {
+        var i = 0;
+        for(let j=1; j<arr.length; j++) {
+            if(arr[i] !== arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    } else {
+        throw new Error("Array is empty");
+    }
+}
+console.log(unique([1,1,2,3,4,5,5,6,7]),'Unique count')
+//Time complexity o(n)
