f83857ad252c1f30970abdcb9d268681cebd8167
Ashish Ahuja <ashishahuja124@gmail.com>
2022-11-14 14:29:57 +0530

    LC27 Remove element

---
+++ src/LeetCode/LC27RemoveElement.js
@@ -0,0 +1,30 @@
+// Remove element 
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+
+function removeEle(nums,val) {
+    if(nums.length > 0) {
+        let i = 0;
+        for(let j = 0; j< nums.length; j++) {
+            if(nums[j] !== val) {
+                 //Swap numbers if current value is not equal to val
+                nums[i] = nums[j];
+                i++;
+            }
+        }console.log(nums)
+        return i;
+    }
+    else {
+        throw new Error("Array is empty");
+    }
+}
+console.log(removeEle([3,2,2,3],3));
+
+//Using filter 
+
+// function remove(nums,val) {
+//     nums = nums.filter((el => el !== val));
+//     console.log(nums);
+//     return nums
+// }
+// console.log(remove([3,2,2,3],3))
