674e2462dfe5c2b40ca11dd2dfa4073d15a58bb5
Ashish Ahuja <ashishahuja124@gmail.com>
2024-02-05 17:50:25 +0530

    LC1 Target sum using multiple pointers

--- src/revision/sumZero.js
+++ src/revision/sumZero.js
@@ -30,3 +30,57 @@ console.log(sumZero([-3, -2, 1, 0, 1, 2, 3]));
 //
 
 //Time complexity o(n2);
+
+//Refactor the code using multiple pointers
+//[-5,-4, -2, 1, 0, 1, 2, 3, 4, 6]) = [-4,4]
+
+var findSumPair = (arr) => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        let sum = arr[left] + arr[right];
+        if(sum === 0) {
+            return [arr[left], arr[right]];
+        }
+        else if(sum > 0) {
+            right--;
+        }
+        else {
+            left ++;
+        }
+    }
+}
+
+
+const res = findSumPair([-5,-4, -2, 1, 0, 1, 2, 3, 4, 6]);
+console.log(res);
+
+//Using multiple pointers
+var twoSum = function(nums, target) {
+    // Create a copy of the original array and sort it
+    const sortedNums = [...nums].sort((a, b) => a - b);
+
+    let left = 0;
+    let right = sortedNums.length - 1;
+
+    while(left < right) {
+        let sum = sortedNums[left] + sortedNums[right];
+        if(sum === target) {
+            // Find the indices in the original array
+            const indexLeft = nums.indexOf(sortedNums[left]);
+            const indexRight = nums.lastIndexOf(sortedNums[right]);
+            return [indexLeft, indexRight];
+        } else if(sum > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    // If no pair is found, return an indicator (e.g., an empty array or null)
+    return [];
+};
+
+const re = twoSum([3, 2, 4], 6);
+console.log(re);
