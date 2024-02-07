3c9b99130c45884abcd547d31a091d34687728d1
Ashish Ahuja <ashishahuja124@gmail.com>
2024-02-07 16:44:46 +0530

    lc283 Move zeroes optimal solution

--- src/LeetCode/LC283MoveZero.js
+++ src/LeetCode/LC283MoveZero.js
@@ -14,4 +14,25 @@ function sortZero(arr) {
     console.log(arr);
 }
 
-console.log(sortZero([0,1,0,3,12]));
+// console.log(sortZero([0,1,0,3,12]));
+//Optimal solution
+function sumZero2(arr) {
+    let insertPosition = 0;
+    for (let i = 0; i < arr.length; i++) {
+        // Fill all non-zero elements
+        if (arr[i] !== 0) {
+            arr[insertPosition] = arr[i];
+            insertPosition++
+        }
+    }
+
+    // Fill the remaining elements with zeroes
+    while (insertPosition < arr.length) {
+        arr[insertPosition] = 0;
+        insertPosition++
+    }
+    
+    console.log(arr);
+}
+
+sumZero2([0, 1, 0, 3, 12]);//[1,3,12,0,0]
