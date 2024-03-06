bac0eb73affcd4752e712ed0a54e4fd4c787aeb7
Ashish Ahuja <ashishahuja124@gmail.com>
2024-03-06 16:25:12 +0530

    LC53maxSubArray sum using multiple pointers /kadene's algo

---
+++ Striver SDE sheet/Arrays/Array Medium/LC53subArraySum.js
@@ -0,0 +1,18 @@
+function maxSubArray(nums) {
+    let temp = 0
+    let max = -Infinity;
+
+    for(let i=0; i< nums.length; i++) {
+        temp += nums[i];
+        console.log(temp,'tempSUm');
+        if(temp > max) {
+            max = temp;
+            console.log(max, 'max');
+        }
+        if(temp < 0) {
+            temp = 0
+        }
+    }return max
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) // 6
