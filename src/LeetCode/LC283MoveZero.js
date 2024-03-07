3a6283de9d576f73922b5bc958c2c4a6c710646b
Ashish Ahuja <ashishahuja124@gmail.com>
2024-03-07 12:42:21 +0530

    striver, LC283 Move zeroes to end brute force

--- src/LeetCode/LC283MoveZero.js
+++ src/LeetCode/LC283MoveZero.js
@@ -35,4 +35,35 @@ function sumZero2(arr) {
     console.log(arr);
 }
 
-sumZero2([0, 1, 0, 3, 12]);//[1,3,12,0,0]
+// sumZero2([0, 1, 0, 3, 12]);//[1,3,12,0,0]
+
+//Brute force 
+
+function sumZero3(arr) {
+    let temp = [];
+
+    //Extract all the non zero elements to temp
+
+    for(let i = 0; i<arr.length; i++) {
+        if(arr[i] != 0) {
+            temp.push(arr[i]);
+        
+        }
+        console.log(temp)
+    }
+
+    let nz = temp.length; //length of non zero
+
+    //copy elements from temp & fill them in first posotions in the array
+    for(let i=0; i< nz; i++) {
+        arr[i] = temp[i];
+    }
+    console.log(arr)
+
+    //Fill the zeroes in remaining position
+    for(let i = nz; i< arr.length; i++) {
+        arr[i] = 0;
+    }
+    console.log(arr)
+}
+sumZero3([0, 1, 0, 3, 12]);//[1,3,12,0,0]
