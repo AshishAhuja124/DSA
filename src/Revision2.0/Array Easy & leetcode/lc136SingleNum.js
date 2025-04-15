80bc40123bbfeceab58eb51b388ef35b8b6b33be
Ashish Ahuja <ashishahuja124@gmail.com>
2025-04-16 00:25:17 +0530

    lc 136 single number using xor

--- src/Revision2.0/Array Easy & leetcode/lc136SingleNum.js
+++ src/Revision2.0/Array Easy & leetcode/lc136SingleNum.js
@@ -1,18 +1,31 @@
-var singleNumber = function(nums) {
-    let counter = {};
-  
-    for (let val of nums) {
-      counter[val] = (counter[val] || 0) + 1;
+var singleNumber = function (nums) {
+  let counter = {};
+
+  for (let val of nums) {
+    counter[val] = (counter[val] || 0) + 1;
+  }
+
+  console.log("Frequency map:", counter);
+
+  for (let val in counter) {
+    if (counter[val] === 1) {
+      return Number(val); // convert back to number
     }
-  
-    console.log("Frequency map:", counter);
-  
-    for (let val in counter) {
-      if (counter[val] === 1) {
-        return Number(val); // convert back to number
-      }
-    }
-  };
-  
-  console.log(singleNumber([2, 2, 1])); // Output: 1
-  
+  }
+};
+
+console.log(singleNumber([2, 2, 1])); // Output: 1
+
+//using xor
+
+function singleNumberXORR(nums) {
+  let xor = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ nums[i];
+  }
+  console.log("XOR result:", xor);
+  return xor;
+}
+
+console.log(singleNumberXORR([2, 2, 1])); // Output: 1
