24ab656dfe341c8ca63f0a5aa745da8df36249f0
Ashish Ahuja <ashishahuja124@gmail.com>
2025-04-23 22:12:39 +0530

    filter polyfill

--- src/javascript/polifills.js
+++ src/javascript/polifills.js
@@ -3,7 +3,7 @@
 var arr = [1,2,3,4,5]
 var modifiedArr = arr.map((x) => x * 2);
 
-console.log(modifiedArr);
+// console.log(modifiedArr);
 
 //Polyfill for map()  array.map(num, i , arr)
 
@@ -16,4 +16,24 @@ Array.prototype.myMap = function(cb) {
 }
 
 var modifiedArrbyMao = arr.myMap((num, i, arr) => num * 2);
-console.log(modifiedArrbyMao) // [2,4,6,8, 10]
+// console.log(modifiedArrbyMao) // [2,4,6,8, 10]
+
+//----------Filter----------------------------------------------
+
+var filterArr = arr.filter((x) => x > 2);
+console.log(filterArr)
+
+//polyfill for filter
+
+Array.prototype.myFilter = function(cb) {
+    let temp = [];
+    for(let i = 0; i< this.length; i++) {
+        if(cb(this[i], i, this)) {
+            temp.push(this[i]);
+        }
+    }
+    return temp;
+}
+
+var myFilterLogic = arr.myFilter(x => x > 3);
+console.log(myFilterLogic)
