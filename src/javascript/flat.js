3678862a36a8601cc9a9fe17abfe2f0e5ed31fb1
Ashish Ahuja <ashishahuja124@gmail.com>
2025-05-07 16:25:31 +0530

    flat custom

--- src/javascript/flat.js
+++ src/javascript/flat.js
@@ -24,3 +24,20 @@ function customFlat(arr, depth = 1) {
 }
 
 console.log(customFlat(arr, 2))
+
+
+const arrFlat = function(arr, depth = 1) {
+    let result = [];
+
+    for(let item of arr) {
+        if(Array.isArray(item) && depth > 0 ){
+            result.push(...arrFlat(item, depth - 1))
+        } else {
+            result.push(item)
+        }
+
+    }
+    return result;
+}
+
+console.log(arrFlat(arr, 2)) //[1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
